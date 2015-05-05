06c0520e072d07d0b3032fff1467c0ad4b1291b8
Jozef <jozefw@sbcglobal.net>
2015-05-05 13:49:45 -0700

    add handle bars and view engine

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+// require handlebars
+var hbs = require('hbs');
+
+// sets the key view engine the value of html
+app.set('view engine', 'html');
+app.engine('hmtl', hbs.__express);
 
 // when theres a get request to the home page send the file in the view folder
 app.get('/', function(request, response){
